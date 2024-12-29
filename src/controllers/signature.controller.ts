6a91270cd0525fcb98b5376367bf37d0454fe102
Florian <79457058+florian-alb@users.noreply.github.com>
2024-12-29 18:56:14 +0100

    add async handler to signature controller

--- src/controllers/signature.controller.ts
+++ src/controllers/signature.controller.ts
@@ -10,100 +10,90 @@ import { RequestHandler } from "express";
 import { IAuthenticatedRequest } from "../types/express";
 import { AppError } from "../types/appError";
 import { errorHandler } from "../utils/errorHandler";
+import { asyncHandler } from "../handlers/asyncHandler";
 
-export const getSignatures: RequestHandler = async (
-  req: IAuthenticatedRequest,
-  res
-) => {
-  const signatures = await getAll(req.user?.id);
-  res.json({
-    success: true,
-    data: signatures,
-  });
-};
-
-export const getSignatureById: RequestHandler = async (
-  req: IAuthenticatedRequest,
-  res,
-  next
-) => {
-  const signature = await getById(req.user?.id, req.params.id);
-
-  if (!signature) {
-    return next(new AppError("Signature not found", 404));
+export const getSignatures: RequestHandler = asyncHandler(
+  async (req: IAuthenticatedRequest, res) => {
+    const signatures = await getAll(req.user?.id);
+    res.json({
+      success: true,
+      data: signatures,
+    });
   }
+);
 
-  res.json({
-    success: true,
-    data: signature,
-  });
-};
+export const getSignatureById: RequestHandler = asyncHandler(
+  async (req: IAuthenticatedRequest, res, next) => {
+    const signature = await getById(req.user?.id, req.params.id);
 
-export const getActiveSignature: RequestHandler = async (
-  req: IAuthenticatedRequest,
-  res,
-  next
-) => {
-  const signature = await getActive(req.user?.id);
+    if (!signature) {
+      return next(new AppError("Signature not found", 404));
+    }
 
-  if (!signature) {
-    return next(new AppError("Active signature not found", 404));
+    res.json({
+      success: true,
+      data: signature,
+    });
   }
+);
 
-  res.json({
-    success: true,
-    data: signature,
-  });
-};
+export const getActiveSignature: RequestHandler = asyncHandler(
+  async (req: IAuthenticatedRequest, res, next) => {
+    const signature = await getActive(req.user?.id);
 
-export const addSignature: RequestHandler = async (
-  req: IAuthenticatedRequest,
-  res,
-  next
-) => {
-  const signature = await add(req.user?.id, req.body);
+    if (!signature) {
+      return next(new AppError("Active signature not found", 404));
+    }
 
-  if (!signature) {
-    return next(new AppError("Error while creating a new signature", 501));
+    res.json({
+      success: true,
+      data: signature,
+    });
   }
+);
 
-  res.status(201).json({
-    success: true,
-    data: signature,
-  });
-};
+export const addSignature: RequestHandler = asyncHandler(
+  async (req: IAuthenticatedRequest, res, next) => {
+    const signature = await add(req.user?.id, req.body);
 
-export const updateSignature: RequestHandler = async (
-  req: IAuthenticatedRequest,
-  res,
-  next
-) => {
-  let signature;
+    if (!signature) {
+      return next(new AppError("Error while creating a new signature", 501));
+    }
 
-  try {
-    signature = await update(req.user?.id, req.params.id, req.body);
-  } catch (err) {
-    return next(errorHandler(err, "Error while updating the signature", 501));
+    res.status(201).json({
+      success: true,
+      data: signature,
+    });
   }
+);
 
-  res.json({
-    success: true,
-    data: signature,
-  });
-};
+export const updateSignature: RequestHandler = asyncHandler(
+  async (req: IAuthenticatedRequest, res, next) => {
+    let signature;
 
-export const deleteSignature: RequestHandler = async (
-  req: IAuthenticatedRequest,
-  res,
-  next
-) => {
-  const hasBeenDeleted = await deleteById(req.user?.id, req.params.id);
-  if (!hasBeenDeleted) {
-    return next(new AppError("Signature not found", 404));
+    try {
+      signature = await update(req.user?.id, req.params.id, req.body);
+    } catch (err) {
+      return next(errorHandler(err, "Error while updating the signature", 501));
+    }
+
+    res.json({
+      success: true,
+      data: signature,
+    });
   }
+);
 
-  res.status(204).json({
-    success: true,
-    message: "Signature deleted",
-  });
-};
+export const deleteSignature: RequestHandler = asyncHandler(
+  async (req: IAuthenticatedRequest, res, next) => {
+    const hasBeenDeleted = await deleteById(req.user?.id, req.params.id);
+    if (!hasBeenDeleted) {
+      return next(new AppError("Signature not found", 404));
+    }
+
+    res.status(204).json({
+      success: true,
+      message: "Signature deleted",
+    });
+  }
+);
