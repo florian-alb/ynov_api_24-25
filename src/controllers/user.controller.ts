9c7f7015812839e171add626acca0045a6251783
Florian <79457058+florian-alb@users.noreply.github.com>
2024-12-29 18:51:56 +0100

    add async handler to user controller

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -12,70 +12,70 @@ import { AppError } from "../types/appError";
 import { errorHandler } from "../utils/errorHandler";
 import { asyncHandler } from "../handlers/asyncHandler";
 
-export const getUser: RequestHandler = async (
-  req: IAuthenticatedRequest,
-  res,
-  next
-) => {
-  const user = await getById(req.user?.id);
+export const getUser: RequestHandler = asyncHandler(
+  async (req: IAuthenticatedRequest, res, next) => {
+    const user = await getById(req.user?.id);
 
-  if (!user) {
-    return next(new AppError("User not found", 404));
-  }
+    if (!user) {
+      return next(new AppError("User not found", 404));
+    }
 
-  res.json({
-    success: true,
-    data: user,
-  });
-};
-
-export const registerUser: RequestHandler = async (req, res, next) => {
-  const { name, emailAddress, password } = req.body;
-  let user;
-
-  try {
-    user = await create({ name, emailAddress, password });
-  } catch (err) {
-    return next(errorHandler(err, "Error while creating a new account", 501));
-  }
-
-  res.status(201).json({
-    success: true,
-    data: user,
-  });
-};
-
-export const loginUser: RequestHandler = async (req, res, next) => {
-  const { emailAddress, password } = req.body;
-
-  try {
-    const token = await login({ emailAddress, password });
-
-    res.status(200).json({
+    res.json({
       success: true,
-      message: "Login successful",
-      token,
+      data: user,
     });
-  } catch {
-    next(new AppError("Invalid credentials", 401));
   }
-};
+);
 
-export const deleteUser: RequestHandler = async (
-  req: IAuthenticatedRequest,
-  res,
-  next
-) => {
-  const hasBeenDeleted = await deleteById(req.user?.id);
-  if (!hasBeenDeleted) {
-    return next(new AppError("User not found", 404));
+export const registerUser: RequestHandler = asyncHandler(
+  async (req, res, next) => {
+    const { name, emailAddress, password } = req.body;
+    let user;
+
+    try {
+      user = await create({ name, emailAddress, password });
+    } catch (err) {
+      return next(errorHandler(err, "Error while creating a new account", 501));
+    }
+
+    res.status(201).json({
+      success: true,
+      data: user,
+    });
   }
+);
 
-  res.status(204).json({
-    success: true,
-    message: "User deleted",
-  });
-};
+export const loginUser: RequestHandler = asyncHandler(
+  async (req, res, next) => {
+    const { emailAddress, password } = req.body;
+
+    try {
+      const token = await login({ emailAddress, password });
+
+      res.status(200).json({
+        success: true,
+        message: "Login successful",
+        token,
+      });
+    } catch {
+      next(new AppError("Invalid credentials", 401));
+    }
+  }
+);
+
+export const deleteUser: RequestHandler = asyncHandler(
+  async (req: IAuthenticatedRequest, res, next) => {
+    const hasBeenDeleted = await deleteById(req.user?.id);
+    if (!hasBeenDeleted) {
+      return next(new AppError("User not found", 404));
+    }
+
+    res.status(204).json({
+      success: true,
+      message: "User deleted",
+    });
+  }
+);
 
 export const updateUser: RequestHandler = asyncHandler(
   async (req: IAuthenticatedRequest, res) => {
